36e3acfc6630361d6365780a882ac3ccf0a62a88
Mateus Neiva <mateus.fneiva@gmail.com>
2026-06-14 12:22:24 -0300

    Update main.ts

--- apps/discord-bot/src/main.ts
+++ apps/discord-bot/src/main.ts
@@ -37,6 +37,15 @@ const player = new Player(client, {
   blockStreamFrom: ["com.discord-player.spotifyextractor"],
 });
 
+player.events.on("error", (queue, error) => {
+  console.error("[error]", error.message, error.stack);
+});
+
+player.events.on("debug", (queue, message) => {
+  console.log(`[Queue Debug ${queue.guild.name}] ${message}`);
+});
+
+
 registerPlayerStreamHooks(player);
 
 async function bootstrap() {
